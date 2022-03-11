039a97c22863ac50bf8502c87fa56b61e6324132
Dhananjay54 <76160215+Dhananjay54@users.noreply.github.com>
2022-03-11 14:48:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-document.getElementById("button").onclick = function () { 
-    const visits=document.getElementById('p_visits').value;
-    const age =document.getElementById('age').value;
-    const date =document.getElementById('date').value;
-    const dep =document.getElementById('dep').value;
-    
-    const errorMsg= new Array(); 
-    const messageHtml = "";      
-        if(dep!= "Paediatrician" && dep!="Ophthalmology")
-        { 
-                errorMsg.push("Department name not valid\n");
-        }           
-        if(date>"1-5-2022" && date<"31-5-2022" ){
-                errorMsg.push("Date not valid\n");
-        }
-        if(isNaN(age) || age<0){
-                errorMsg.push("Age not valid\n");
-        }
-        if(isNaN(visits) || visits<0){
-                errorMsg.push("Number of visits not valid\n");
-        }
-    errorMsg.forEach(function (message) {
-    messageHtml += "<li>" + message + "</li>";
-});
+document.getElementById("btn").onclick = function () { 
+        var visits=document.getElementById('p_visits').value;
+        var age=document.getElementById('age').value;
+        var date=document.getElementById('date').value;
+        var dep=document.getElementById('dep').value;
+        
+        var errorMsg=new Array(); 
+        var messageHtml = "";      
+            if(dep!= "Pediatrician" && dep!="Ophthalmology"){ 
+                    errorMsg.push("Department name not valid\n");
+            }           
+            if(date>"1-5-2022" && date<"31-5-2022" ){
+                    errorMsg.push("Date not valid\n");
+            }
+            if(isNaN(age) || age<0){
+                    errorMsg.push("Age not valid\n");
+            }
+            if(isNaN(visits) || visits<0){
+                    errorMsg.push("Number of visits not valid\n");
+            }
+        errorMsg.forEach(function (message) {
+        messageHtml += "<li>" + message + "</li>";
+    });
 
-document.getElementById("error").innerHTML = messageHtml;
-}
+    document.getElementById("error").innerHTML = messageHtml;
+    }
